8fc9ab73599b97a2412f25705ae89f4015d3b7b2
Salim <vu2lid@gmail.com>
2013-07-10 22:33:28 +0000

    Added reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+
+var buf = fs.readFileSync('index.html');
+var out = buf.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
